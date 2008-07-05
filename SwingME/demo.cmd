aa8329eef7ad557b3bb0bf0d8e1368d258b60899
yuranet <yuranet@50c89ffe-5e77-4af1-8422-6b4edaf2f3c0>
2008-07-05 12:49:46 +0000

    javadoc gen

--- SwingME/demo.cmd
+++ SwingME/demo.cmd
@@ -1,2 +1,2 @@
-java -cp lib/me4se.jar;dist/SwingME.jar org.me4se.MIDletRunner net.yura.mobile.test.MyMidlet
+java -cp lib/me4se.jar;dist/SwingME.jar org.me4se.MIDletRunner -width 176 -height 243 net.yura.mobile.test.MyMidlet
 pause
---
+++ SwingME/makejavadoc.cmd
@@ -0,0 +1,2 @@
+javadoc -use -version -author -nodeprecated -package -sourcepath src/ -classpath lib/me4se.jar -d doc -doctitle "yura.net Swing ME" -windowtitle "yura.net Swing ME" -link http://java.sun.com/j2se/1.4.2/docs/api/ net.yura.mobile.gui net.yura.mobile.gui.border net.yura.mobile.gui.cellrenderer net.yura.mobile.gui.components net.yura.mobile.gui.layout net.yura.mobile.util
+pause
