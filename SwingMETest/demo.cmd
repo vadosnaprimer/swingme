4f7c339b9a4fa981197ca0054a6c9084e85292c8
yuranet <yuranet@50c89ffe-5e77-4af1-8422-6b4edaf2f3c0>
2009-02-05 02:20:45 +0000

    Test App added back

---
+++ SwingMETest/demo.cmd
@@ -0,0 +1,3 @@
+rem N70
+java -cp lib/me4se.jar;lib/kxml2.jar;dist/SwingME.jar org.me4se.MIDletRunner -width 176 -height 243 net.yura.mobile.test.MyMidlet
+pause
---
+++ SwingMETest/demo_xlarge.cmd
@@ -0,0 +1,3 @@
+rem N80
+java -cp lib/me4se.jar;lib/SwingME.jar;dist/SwingMETest.jar org.me4se.MIDletRunner -width 352 -height 450 net.yura.mobile.test.MyMidlet
+pause
