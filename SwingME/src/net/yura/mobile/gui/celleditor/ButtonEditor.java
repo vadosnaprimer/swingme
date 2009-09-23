9aed27e4500270653ce7eb79be87780b031f8704
yuranet <yuranet@50c89ffe-5e77-4af1-8422-6b4edaf2f3c0>
2009-09-23 10:35:19 +0000

    button editor

---
+++ SwingME/src/net/yura/mobile/gui/celleditor/ButtonEditor.java
@@ -0,0 +1,88 @@
+/*
+ *  This file is part of 'yura.net Swing ME'.
+ *
+ *  'yura.net Swing ME' is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  'yura.net Swing ME' is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with 'yura.net Swing ME'. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.yura.mobile.gui.celleditor;
+
+import net.yura.mobile.gui.cellrenderer.ListCellRenderer;
+import net.yura.mobile.gui.components.Button;
+import net.yura.mobile.gui.components.Component;
+import net.yura.mobile.gui.components.List;
+import net.yura.mobile.gui.components.Table;
+import net.yura.mobile.gui.plaf.Style;
+
+/**
+ * @author Yura Mamyrin
+ */
+class ButtonEditor extends Button implements ListCellRenderer, TableCellEditor {
+
+        private int state;
+        private Object obj;
+        private String label;
+
+        public ButtonEditor(String s) {
+            super(s);
+
+            label = s;
+        }
+
+        public Component getListCellRendererComponent(List list, Object arg1, int arg2, boolean isSelected, boolean cellHasFocus) {
+
+            if (label==null) {
+                setValue(arg1);
+            }
+
+            state=Style.ALL;
+            if ( list!=null ) {
+                if (list.isFocusable()) {
+                    //state |= Style.ENABLED;
+                }
+                else {
+                    state |= Style.DISABLED;
+                }
+            }
+            if (cellHasFocus) {
+                state |= Style.FOCUSED;
+            }
+//            if (isSelected) {
+//                state |= Style.SELECTED;
+//            }
+
+            return this;
+        }
+
+        public int getCurrentState() {
+            if (parent!=null) {
+                return super.getCurrentState();
+            }
+            return state;
+        }
+
+
+        public Component getTableCellEditorComponent(Table arg0, Object arg1, boolean arg2, int arg3, int arg4) {
+
+            if (label==null) {
+                setValue(arg1);
+            }
+
+            obj = arg1;
+            return this;
+        }
+        public Object getCellEditorValue() {
+            return obj;
+        }
+
+}
