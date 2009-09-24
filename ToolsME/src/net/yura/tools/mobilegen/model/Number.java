1b1ff8a8d077d210db31354c2331060b532af171
exeus <exeus@50c89ffe-5e77-4af1-8422-6b4edaf2f3c0>
2009-09-24 09:46:16 +0000

    Fixed missing file reference and added test.proto

---
+++ ToolsME/src/net/yura/tools/mobilegen/model/Number.java
@@ -0,0 +1,27 @@
+package net.yura.tools.mobilegen.model;
+
+public class Number {
+
+    private String number;
+    private int type;
+    
+    public String getNumber()
+    {
+        return this.number;
+    }
+
+    public int getType()
+    {
+        return this.type;
+    }
+    
+    public void setNumber(String number)
+    {
+        this.number = number;
+    }
+    
+    public void setType(int type)
+    {
+        this.type = type;
+    }
+}
