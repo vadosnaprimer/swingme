e847f4e52ca0d9ee6dba1df3a4519c48cac92b7c
yuranet <yuranet@50c89ffe-5e77-4af1-8422-6b4edaf2f3c0>
2009-10-08 16:43:58 +0000

    removing of none working proto stuff

--- UtilME/src/net/yura/mobile/io/ProtoUtil.java
+++ UtilME/src/net/yura/mobile/io/ProtoUtil.java
@@ -36,7 +36,7 @@ public class ProtoUtil {
 
         out2.flush();
         //out.write(message);
-System.out.println("save size "+size);
+//System.out.println("save size "+size);
         return size;
     }
 
@@ -58,12 +58,12 @@ System.out.println("save size "+size);
             int tag = in2.readTag();
             int fieldNo = WireFormat.getTagFieldNumber(tag);
             int wireType = WireFormat.getTagWireType(tag);
-    System.out.println("read field "+fieldNo );
-    System.out.println("wire type "+wireType );
+    //System.out.println("read field "+fieldNo );
+    //System.out.println("wire type "+wireType );
 
             if (fieldNo == OBJECT_TYPE) {
                 type = in2.readInt32();
-                System.out.println("object type "+type);
+                //System.out.println("object type "+type);
             }
             else if (fieldNo == OBJECT_VALUE) {
                 if (type==-1) {
@@ -72,9 +72,9 @@ System.out.println("save size "+size);
 
                 int size = in2.readBytesSize();
                 int lim = in2.pushLimit(size);
-                System.out.println("object size "+size);
+                //System.out.println("object size "+size);
                 obj = decodeObject(in2,type);
-                System.out.println("object "+obj);
+                //System.out.println("object "+obj);
                 in2.popLimit(lim);
 
             }
@@ -83,7 +83,7 @@ System.out.println("save size "+size);
         return obj;
     }
 
-    private Object decodeObject(CodedInputStream in2,int type) throws IOException {
+    protected Object decodeObject(CodedInputStream in2,int type) throws IOException {
 
         switch (type) {
             case BinUtil.TYPE_VECTOR: return decodeVector(in2);
@@ -102,8 +102,8 @@ System.out.println("save size "+size);
             int tag = in2.readTag();
             int fieldNo = WireFormat.getTagFieldNumber(tag);
             int wireType = WireFormat.getTagWireType(tag);
-    System.out.println("read field "+fieldNo );
-    System.out.println("wire type "+wireType );
+    //System.out.println("read field "+fieldNo );
+    //System.out.println("wire type "+wireType );
 
             if (fieldNo == DEFAULT_FIELD) {
                 simple = readSimple(in2,type);
@@ -138,14 +138,14 @@ System.out.println("save size "+size);
             int tag = in2.readTag();
             int fieldNo = WireFormat.getTagFieldNumber(tag);
             int wireType = WireFormat.getTagWireType(tag);
-    System.out.println("read field "+fieldNo );
-    System.out.println("wire type "+wireType );
+    //System.out.println("read field "+fieldNo );
+    //System.out.println("wire type "+wireType );
 
             if (fieldNo == VECTOR_ELEMENT) {
 
                 int size = in2.readBytesSize();
                 int lim = in2.pushLimit(size);
-                System.out.println("object size "+size);
+                //System.out.println("object size "+size);
                 Object obj = decodeAnonymousObject(in2);
                 vector.addElement(obj);
                 in2.popLimit(lim);
@@ -164,8 +164,8 @@ System.out.println("save size "+size);
             final int tag = in2.readTag();
             final int fieldNo = WireFormat.getTagFieldNumber(tag);
             final int wireType = WireFormat.getTagWireType(tag);
-    System.out.println("read field "+fieldNo );
-    System.out.println("wire type "+wireType );
+    //System.out.println("read field "+fieldNo );
+    //System.out.println("wire type "+wireType );
 
             if (fieldNo == KEY_VALUE) {
 
@@ -179,14 +179,14 @@ System.out.println("save size "+size);
                     final int tag2 = in2.readTag();
                     final int fieldNo2 = WireFormat.getTagFieldNumber(tag2);
                     final int wireType2 = WireFormat.getTagWireType(tag2);
-            System.out.println("read field "+fieldNo2 );
-            System.out.println("wire type "+wireType2 );
+            //System.out.println("read field "+fieldNo2 );
+            //System.out.println("wire type "+wireType2 );
 
                     if (fieldNo2 == HASHTABLE_KEY) {
 
                         int size2 = in2.readBytesSize();
                         int lim2 = in2.pushLimit(size2);
-                        System.out.println("object size "+size2);
+                        //System.out.println("object size "+size2);
                         key = decodeAnonymousObject(in2);
                         in2.popLimit(lim2);
 
@@ -195,7 +195,7 @@ System.out.println("save size "+size);
 
                         int size2 = in2.readBytesSize();
                         int lim2 = in2.pushLimit(size2);
-                        System.out.println("object size "+size2);
+                        //System.out.println("object size "+size2);
                         value = decodeAnonymousObject(in2);
                         in2.popLimit(lim2);
                     }
@@ -214,126 +214,62 @@ System.out.println("save size "+size);
 
     // #########################################################################
 
-    private int computeObjectSize(Object obj) {
+    protected int computeObjectSize(Object obj,int type) {
 
-        if (obj instanceof Double) {
-            return CodedOutputStream.computeDoubleSize(DEFAULT_FIELD, ((Double)obj).doubleValue() );
-        }
-        else if (obj instanceof Integer) {
-            return CodedOutputStream.computeInt32Size(DEFAULT_FIELD,((Integer)obj).intValue());
-        }
-        else if (obj instanceof String) {
-            return CodedOutputStream.computeStringSize(DEFAULT_FIELD, (String)obj );
-        }
-        else if (obj instanceof Float) {
-            return CodedOutputStream.computeFloatSize(DEFAULT_FIELD, ((Float)obj).floatValue());
-        }
-        else if (obj instanceof Short) {
-            return CodedOutputStream.computeInt32Size(DEFAULT_FIELD,((Short)obj).shortValue());
-        }
-        else if (obj instanceof Long) {
-            return CodedOutputStream.computeInt64Size(DEFAULT_FIELD,((Long)obj).longValue());
-        }
-        else if (obj instanceof Boolean) {
-            return CodedOutputStream.computeBoolSize(DEFAULT_FIELD, ((Boolean)obj).booleanValue() );
-        }
-        else if (obj instanceof Byte) {
-            return CodedOutputStream.computeInt32Size(DEFAULT_FIELD,((Byte)obj).byteValue());
-        }
-        else if (obj instanceof Object[]) {
-            return computeArraySize( (Object[])obj );
-        }
-        else if (obj instanceof Hashtable) {
-            return computeHashtableSize( (Hashtable)obj );
-        }
-        else if (obj instanceof byte[]) {
-            return CodedOutputStream.computeBytesSize(DEFAULT_FIELD, ((byte[])obj).length );
-        }
-        else if (obj instanceof Character) {
-            return CodedOutputStream.computeInt32Size(DEFAULT_FIELD,((Character)obj).charValue());
-        }
-        else if (obj instanceof Vector) {
-            return computeVectorSize( (Vector)obj );
-        }
-        else if (obj == null) {
-            return 0; // TODO is this right??
-        }
-        else {
-            throw new RuntimeException();
-        }
-    }
-
-    private void encodeObject(CodedOutputStream out, Object obj) throws IOException {
-
-        if (obj instanceof Double) {
-            out.writeDouble(DEFAULT_FIELD,((Double)obj).doubleValue());
-        }
-        else if (obj instanceof Integer) {
-            out.writeInt32(DEFAULT_FIELD,((Integer)obj).intValue());
-        }
-        else if (obj instanceof Vector) {
-            encodeVector( out, (Vector)obj );
-        }
-        else if (obj instanceof String) {
-            out.writeString(DEFAULT_FIELD, (String)obj );
-        }
-        else if (obj instanceof Float) {
-            out.writeFloat(DEFAULT_FIELD, ((Float)obj).floatValue() );
-        }
-        else if (obj instanceof Short) {
-            out.writeInt32(DEFAULT_FIELD,((Short)obj).shortValue());
-        }
-        else if (obj instanceof Long) {
-            out.writeInt64(DEFAULT_FIELD,((Long)obj).longValue());
-        }
-        else if (obj instanceof Boolean) {
-            out.writeBool(DEFAULT_FIELD, ((Boolean)obj).booleanValue() );
-        }
-        else if (obj instanceof Byte) {
-            out.writeInt32(DEFAULT_FIELD, ((Byte)obj).byteValue());
-        }
-        else if (obj instanceof Object[]) {
-            encodeArray( out, (Object[])obj );
-        }
-        else if (obj instanceof Hashtable) {
-            encodeHashtable( out, (Hashtable)obj );
-        }
-        else if (obj instanceof byte[]) {
-            out.writeBytes(DEFAULT_FIELD, (byte[])obj);
-        }
-        else if (obj instanceof Character) {
-            out.writeInt32(DEFAULT_FIELD,((Character)obj).charValue());
-        }
-        else if (obj == null) {
-            // nothing??
-        }
-        else {
-            throw new IOException();
+        switch(type) {
+            case BinUtil.TYPE_INTEGER: return CodedOutputStream.computeInt32Size(DEFAULT_FIELD,((Integer)obj).intValue());
+            case BinUtil.TYPE_DOUBLE: return CodedOutputStream.computeDoubleSize(DEFAULT_FIELD, ((Double)obj).doubleValue() );
+            case BinUtil.TYPE_VECTOR: return computeVectorSize( (Vector)obj );
+            case BinUtil.TYPE_STRING: return CodedOutputStream.computeStringSize(DEFAULT_FIELD, (String)obj );
+            case BinUtil.TYPE_BOOLEAN: return CodedOutputStream.computeBoolSize(DEFAULT_FIELD, ((Boolean)obj).booleanValue() );
+            case BinUtil.TYPE_BYTE: return CodedOutputStream.computeInt32Size(DEFAULT_FIELD,((Byte)obj).byteValue());
+            case BinUtil.TYPE_CHARACTER: return CodedOutputStream.computeInt32Size(DEFAULT_FIELD,((Character)obj).charValue());
+            case BinUtil.TYPE_SHORT: return CodedOutputStream.computeInt32Size(DEFAULT_FIELD,((Short)obj).shortValue());
+            case BinUtil.TYPE_LONG: return CodedOutputStream.computeInt64Size(DEFAULT_FIELD,((Long)obj).longValue());
+            case BinUtil.TYPE_ARRAY: return computeArraySize( (Object[])obj );
+            case BinUtil.TYPE_HASHTABLE: return computeHashtableSize( (Hashtable)obj );
+            case BinUtil.TYPE_FLOAT: return CodedOutputStream.computeFloatSize(DEFAULT_FIELD, ((Float)obj).floatValue());
+            case BinUtil.TYPE_BYTE_ARRAY: return CodedOutputStream.computeBytesSize(DEFAULT_FIELD, ((byte[])obj).length );
+            case BinUtil.TYPE_NULL: return 0; // nothing
+            default: throw new RuntimeException();
         }
 
     }
 
+    protected void encodeObject(CodedOutputStream out, Object obj,int type) throws IOException {
 
+        switch(type) {
+            case BinUtil.TYPE_INTEGER: out.writeInt32(DEFAULT_FIELD,((Integer)obj).intValue()); break;
+            case BinUtil.TYPE_DOUBLE: out.writeDouble(DEFAULT_FIELD,((Double)obj).doubleValue()); break;
+            case BinUtil.TYPE_VECTOR: encodeVector( out, (Vector)obj ); break;
+            case BinUtil.TYPE_STRING: out.writeString(DEFAULT_FIELD, (String)obj ); break;
+            case BinUtil.TYPE_BOOLEAN: out.writeBool(DEFAULT_FIELD, ((Boolean)obj).booleanValue() ); break;
+            case BinUtil.TYPE_BYTE: out.writeInt32(DEFAULT_FIELD, ((Byte)obj).byteValue()); break;
+            case BinUtil.TYPE_CHARACTER: out.writeInt32(DEFAULT_FIELD,((Character)obj).charValue()); break;
+            case BinUtil.TYPE_SHORT: out.writeInt32(DEFAULT_FIELD,((Short)obj).shortValue()); break;
+            case BinUtil.TYPE_LONG: out.writeInt64(DEFAULT_FIELD,((Long)obj).longValue()); break;
+            case BinUtil.TYPE_ARRAY: encodeArray( out, (Object[])obj ); break;
+            case BinUtil.TYPE_HASHTABLE: encodeHashtable( out, (Hashtable)obj ); break;
+            case BinUtil.TYPE_FLOAT: out.writeFloat(DEFAULT_FIELD, ((Float)obj).floatValue() ); break;
+            case BinUtil.TYPE_BYTE_ARRAY: out.writeBytes(DEFAULT_FIELD, (byte[])obj); break;
+            case BinUtil.TYPE_NULL: break; // nothing
+            default: throw new IOException();
+        }
 
-    private int computeAnonymousObjectSize(Object obj) {
+    }
 
-        int size1 = CodedOutputStream.computeInt32Size(OBJECT_TYPE, getObjectTypeEnum(obj) );
-        int size2 = CodedOutputStream.computeBytesSize(OBJECT_VALUE, computeObjectSize(obj) );
-
+    public int computeAnonymousObjectSize(Object obj) {
+        int type = getObjectTypeEnum(obj);
+        int size1 = CodedOutputStream.computeInt32Size(OBJECT_TYPE, type );
+        int size2 = CodedOutputStream.computeBytesSize(OBJECT_VALUE, computeObjectSize(obj,type) );
         return size1+size2;
-
     }
     private void encodeAnonymousObject(CodedOutputStream out, Object object) throws IOException {
-
-        out.writeInt32(OBJECT_TYPE,  getObjectTypeEnum(object) );
-        out.writeBytes(OBJECT_VALUE, computeObjectSize(object) );
-        encodeObject(out,object);
-
+        int type = getObjectTypeEnum(object);
+        out.writeInt32(OBJECT_TYPE, type );
+        out.writeBytes(OBJECT_VALUE, computeObjectSize(object,type) );
+        encodeObject(out,object,type);
     }
-
-
-
-
 
     private int computeVectorSize(Vector vector) {
         int size=0;
@@ -413,7 +349,7 @@ System.out.println("save size "+size);
         }
     }
 
-    public int getObjectTypeEnum(Object obj) {
+    protected int getObjectTypeEnum(Object obj) {
 
         if (obj instanceof Double) {
             return BinUtil.TYPE_DOUBLE;
