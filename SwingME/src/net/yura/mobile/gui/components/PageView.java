1f4f830b48e1ddd6c3ddf8f0db476c23749308ff
janeroski <janeroski@50c89ffe-5e77-4af1-8422-6b4edaf2f3c0>
2010-06-18 16:15:14 +0000

    PageView: Update source control More fixes...

--- SwingME/src/net/yura/mobile/gui/components/PageView.java
+++ SwingME/src/net/yura/mobile/gui/components/PageView.java
@@ -9,50 +9,48 @@ import net.yura.mobile.gui.KeyEvent;
 public class PageView extends ScrollPane {
 
     Vector model;
-    int currentPanel;
+    int currentViewIdx;
 
     public PageView(Vector panels) {
         model = panels;
 
         setMode( ScrollPane.MODE_NONE );
 
-//        add((Component)model.firstElement());
-//        currentPanel = 0;
-
-        // TODO: REVERT THIS
-        add((Component)model.elementAt(1));
-        currentPanel = 1;
-
-
+        add((Component)model.firstElement());
+        currentViewIdx = 0;
 
         setBounceMode(BOUNCE_HORIZONTAL);
-
     }
 
     // Override
-    public void paint(Graphics2D g) {
-        super.paint(g);
+    public void paintChildren(Graphics2D g) {
 
-        Panel currPanel = (Panel) model.elementAt(currentPanel);
+        Component currView = (Component) model.elementAt(currentViewIdx);
 
+        if (!currView.consumesMotionEvents()) { // TODO: How to detect that the component is in "pinch mode"?
 
-        // here we want to draw the prev or next panel in the model
-        if (currentPanel > 0) {
-            Panel prevPanel = (Panel) model.elementAt(currentPanel - 1);
+            int currViewPosX = currView.posX; //TODO: Avoid Thread access...
 
-            int prevPanelPosX = -prevPanel.getWidth() + currPanel.posX;
-            g.translate(prevPanelPosX, 0);
-            prevPanel.paint(g);
-            g.translate(-prevPanelPosX, 0);
+            // here we want to draw the prev or next panel in the model
+            if (currentViewIdx > 0) {
+                Component prevView = (Component) model.elementAt(currentViewIdx - 1);
+
+                int prevViewPosX = currViewPosX - prevView.getWidth();
+                g.translate(prevViewPosX, 0);
+                prevView.paint(g);
+                g.translate(-prevViewPosX, 0);
+            }
+
+            if (currentViewIdx + 1 < model.size()) {
+                Component nextView = (Component) model.elementAt(currentViewIdx + 1);
+                int nextViewPosX = currViewPosX + currView.getWidth();
+                g.translate(nextViewPosX, 0);
+                nextView.paint(g);
+                g.translate(-nextViewPosX, 0);
+            }
         }
 
-        if (currentPanel + 1 < model.size()) {
-            Panel nextPanel = (Panel) model.elementAt(currentPanel + 1);
-            int nextPanelPosX = getViewPortWidth() + currPanel.posX;
-            g.translate(nextPanelPosX, 0);
-            nextPanel.paint(g);
-            g.translate(-nextPanelPosX, 0);
-        }
+        super.paintChildren(g);
 
         g.setColor(0xFF000000);
         g.drawLine(getViewPortWidth() / 2, 0, getViewPortWidth() / 2, getHeight());
@@ -63,32 +61,41 @@ public class PageView extends ScrollPane {
     public void setSize(int w, int h) {
         super.setSize(w, h);
 
-        // TODO: How to calculate the size of the components (panels??) that are not being displayed?
+        // TODO: How to calculate the size of the components that are not being displayed?
         for (int i = 0; i < model.size(); i++) {
-            Panel panel = (Panel) model.elementAt(i);
-            panel.validate();
-            panel.setSize(getViewPortWidth(), getViewPortHeight());
+            Component view = (Component) model.elementAt(i);
+            view.validate();
+            view.setSize(getViewPortWidth(), getViewPortHeight());
         }
     }
 
     // Override
     public void processMouseEvent(int type, int pointX, int pointY, KeyEvent keys) {
 
+        System.out.println("PageView:processMouseEvent");
+
         if (type == DesktopPane.RELEASED) {
 
+            int viewPortW = getViewPortWidth();
 
-            Panel currPanel = (Panel) model.elementAt(currentPanel);
+            Component currView = (Component) model.elementAt(currentViewIdx);
 
-            if (currentPanel > 0) {
+            System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
 
-                if (currPanel.posX > getViewPortWidth() / 2) {
+            int velocityX = getDragVelocity(true);
+            System.out.println("VEL = " + velocityX + " VW = " + viewPortW);
+
+            if (currentViewIdx > 0) {
+                if (velocityX > viewPortW || currView.posX > viewPortW / 2) {
                     goPrev();
+                    resetDragSpeed();
                 }
             }
 
-            if (currentPanel + 1 < model.size()) {
-                if (currPanel.posX + getViewPortWidth() < getViewPortWidth() / 2) {
+            if (currentViewIdx + 1 < model.size()) {
+                if (velocityX < -viewPortW || currView.posX + currView.getWidth() < viewPortW / 2) {
                     goNext();
+                    resetDragSpeed();
                 }
             }
         }
@@ -98,29 +105,47 @@ public class PageView extends ScrollPane {
 
     void goNext() {
 
-        Component old = (Component)model.elementAt(currentPanel);
+        Component old = (Component)model.elementAt(currentViewIdx);
+        int oldX = old.getX() + old.getWidth();
 
-        currentPanel++;
+        currentViewIdx++;
 
-        Component comp = (Component)model.elementAt(currentPanel);
+        Component comp = (Component)model.elementAt(currentViewIdx);
+
 
         add(comp);
 
-        comp.setLocation(old.getX() + getWidth(), old.getY());
+        //TODO: How to reset the size of the components?
+        old.validate();
+        old.setSize(getViewPortWidth(), getViewPortHeight());
+
+        comp.validate();
+        comp.setSize(getViewPortWidth(), getViewPortHeight());
+
+
+        comp.setLocation(oldX, old.getY());
     }
 
     void goPrev() {
 
-        Component old = (Component)model.elementAt(currentPanel);
+        Component old = (Component)model.elementAt(currentViewIdx);
+        int oldX = old.getX() - getWidth();
 
-        currentPanel--;
+        currentViewIdx--;
 
-        Component comp = (Component)model.elementAt(currentPanel);
+        Component comp = (Component)model.elementAt(currentViewIdx);
+
 
         add(comp);
 
-        comp.setLocation(old.getX() - getWidth(), old.getY());
+        //TODO: How to reset the size of the components?
+        old.validate();
+        old.setSize(getViewPortWidth(), getViewPortHeight());
 
+        comp.validate();
+        comp.setSize(getViewPortWidth(), getViewPortHeight());
+
+        comp.setLocation(oldX, old.getY());
     }
 
 
