a9a5ae07c139919ba0d07e6c628d2ae8e6a0ee3d
yuranet <yuranet@50c89ffe-5e77-4af1-8422-6b4edaf2f3c0>
2010-11-05 10:57:22 +0000

    helper border

---
+++ AndroidME/src_SwingME_plaf/net/yura/android/plaf/BorderWithDivider.java
@@ -0,0 +1,50 @@
+package net.yura.android.plaf;
+
+import net.yura.mobile.gui.Graphics2D;
+import net.yura.mobile.gui.border.Border;
+import net.yura.mobile.gui.components.Component;
+
+public class BorderWithDivider implements Border {
+
+    Border border,divider;
+
+    public BorderWithDivider(Border border,Border divider) {
+        this.border = border;
+        this.divider = divider;
+    }
+
+    public void paintBorder(Component c, Graphics2D g, int width, int height) {
+
+        border.paintBorder(c, g, width, height);
+
+        int x = divider.getLeft() - getLeft();
+        int y = height + getBottom();
+        g.translate(x, y);
+        divider.paintBorder(c, g, width
+                +getLeft()-divider.getLeft()
+                +getRight()-divider.getRight()
+                , 0);
+        g.translate(-x, -y);
+    }
+
+    public int getTop() {
+        return border.getTop();
+    }
+
+    public int getBottom() {
+        return border.getBottom() + divider.getTop();
+    }
+
+    public int getRight() {
+        return border.getRight();
+    }
+
+    public int getLeft() {
+        return border.getLeft();
+    }
+
+    public boolean isBorderOpaque() {
+        return border.isBorderOpaque();
+    }
+
+}
