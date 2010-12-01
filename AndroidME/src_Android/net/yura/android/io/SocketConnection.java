35826199421da520e2d06d4a201c70e2710fd79e
janeroski <janeroski@50c89ffe-5e77-4af1-8422-6b4edaf2f3c0>
2010-12-01 11:00:08 +0000

    Socket does not throw IOException when reading from it, and network connection is lost - Workaround for Android Issues 6144, 7935, 7933

--- AndroidME/src_Android/net/yura/android/io/SocketConnection.java
+++ AndroidME/src_Android/net/yura/android/io/SocketConnection.java
@@ -29,20 +29,41 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.lang.ref.WeakReference;
 import java.net.Socket;
+import java.util.Vector;
+
+import net.yura.android.AndroidMeActivity;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.NetworkInfo.State;
 
 public class SocketConnection implements javax.microedition.io.SocketConnection {
 
-	protected Socket socket;
+
+    private static ConnectivityBroadcastReceiver socketBroadcastReceiver;
+
+    protected Socket socket;
 
 	public SocketConnection() {
+	    //#debug debug
+	    System.out.println(">>> SocketConnection: Constructor()");
+
+	    ConnectivityBroadcastReceiver.addSocketConnection(this);
 	}
 
 	public SocketConnection(String host, int port) throws IOException {
+	    this();
 		this.socket = new Socket(host, port);
 	}
 
 	public SocketConnection(Socket socket) {
+	    this();
 		this.socket = socket;
 	}
 
@@ -84,31 +105,19 @@ public class SocketConnection implements javax.microedition.io.SocketConnection 
 			throw new IOException();
 		}
 		switch (option) {
-		case DELAY:
-			if (socket.getTcpNoDelay()) {
-				return 1;
-			} else {
-				return 0;
-			}
-		case LINGER:
-			int value = socket.getSoLinger();
-			if (value == -1) {
-				return 0;
-			} else {
-				return value;
-			}
-		case KEEPALIVE:
-			if (socket.getKeepAlive()) {
-				return 1;
-			} else {
-				return 0;
-			}
-		case RCVBUF:
-			return socket.getReceiveBufferSize();
-		case SNDBUF:
-			return socket.getSendBufferSize();
-		default:
-			throw new IllegalArgumentException();
+    		case DELAY:
+    		    return (socket.getTcpNoDelay()) ? 1 : 0;
+    		case LINGER:
+    			int value = socket.getSoLinger();
+    			return (value == -1) ? 0 : value;
+    		case KEEPALIVE:
+    		    return (socket.getKeepAlive()) ? 1 : 0;
+    		case RCVBUF:
+    			return socket.getReceiveBufferSize();
+    		case SNDBUF:
+    			return socket.getSendBufferSize();
+    		default:
+    			throw new IllegalArgumentException();
 		}
 	}
 
@@ -119,28 +128,16 @@ public class SocketConnection implements javax.microedition.io.SocketConnection 
 		}
 		switch (option) {
 		case DELAY:
-			int delay;
-			if (value == 0) {
-				delay = 0;
-			} else {
-				delay = 1;
-			}
-			socket.setTcpNoDelay(delay == 0 ? false : true);
+			socket.setTcpNoDelay(value != 0);
 			break;
 		case LINGER:
 			if (value < 0) {
 				throw new IllegalArgumentException();
 			}
-			socket.setSoLinger(value == 0 ? false : true, value);
+			socket.setSoLinger(value != 0, value);
 			break;
 		case KEEPALIVE:
-			int keepalive;
-			if (value == 0) {
-				keepalive = 0;
-			} else {
-				keepalive = 1;
-			}
-			socket.setKeepAlive(keepalive == 0 ? false : true);
+			socket.setKeepAlive(value != 0);
 			break;
 		case RCVBUF:
 			if (value <= 0) {
@@ -162,6 +159,17 @@ public class SocketConnection implements javax.microedition.io.SocketConnection 
 	public void close() throws IOException {
 		// TODO fix differences between Java ME and Java SE
 
+	    //#debug debug
+        System.out.println(">>> SocketConnection: close()");
+
+	    try {
+	        socket.shutdownInput();
+        } catch (Throwable e) {}
+
+        try {
+            socket.shutdownOutput();
+        } catch (Throwable e) {}
+
 		socket.close();
 	}
 
@@ -181,4 +189,109 @@ public class SocketConnection implements javax.microedition.io.SocketConnection 
 		return new DataOutputStream(openOutputStream());
 	}
 
+
+	// ----------------- SocketBroadcastReceiver INNER CLASS --------------------
+	/**
+	 *  See Android Issues 6144, 7935, 7933, etc. The socket does not throw IOException
+	 * when a thread is reading from it (and is blocked), and the connection is drop. It just
+	 * hangs forever reading... Writing does not throw any exception either.
+	 *  To work around this, we keep a list of all open socket connections (in weak refs)
+	 *  and we listen for CONNECTIVITY broadcast events. When the connection is dropped,
+	 *  changes status or there is a new active connection (e.g. moving from WIFI to
+	 *  mobile), we close all socket connections.
+	 */
+	static class ConnectivityBroadcastReceiver extends BroadcastReceiver {
+
+        static private synchronized void addSocketConnection(SocketConnection socket) {
+            if (socketBroadcastReceiver == null) {
+                socketBroadcastReceiver = new ConnectivityBroadcastReceiver();
+            }
+
+            socketBroadcastReceiver.cleanSocketConnections(false); // Clean weak refs
+            socketBroadcastReceiver.socketWeakList.add(new WeakReference<SocketConnection>(socket));
+        }
+
+        private Vector<WeakReference<SocketConnection>> socketWeakList = new Vector<WeakReference<SocketConnection>>();
+        private int networkType;
+        private State networkState;
+
+        private ConnectivityBroadcastReceiver() {
+            updateConnectivity(false); // Initialize
+
+            //#debug debug
+            System.out.println(">>> SocketBroadcastReceiver: registerReceiver");
+
+            // Listen for Network events
+            IntentFilter intentFilter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+            AndroidMeActivity.DEFAULT_ACTIVITY.registerReceiver(this, intentFilter);
+        }
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            //#debug debug
+            System.out.println(">>> SocketBroadcastReceiver: received " + intent.getAction() + ": " + intent.getExtras());
+            updateConnectivity(true);
+        }
+
+        private void updateConnectivity(boolean closeConnections) {
+            ConnectivityManager connectivityManager = (ConnectivityManager) AndroidMeActivity.DEFAULT_ACTIVITY.getSystemService(Context.CONNECTIVITY_SERVICE);
+            if (connectivityManager != null) {
+                NetworkInfo netInfo = connectivityManager.getActiveNetworkInfo();
+
+                if (netInfo == null) {
+                    cleanSocketConnections(closeConnections);
+                }
+                else {
+                    State state = netInfo.getState();
+                    int type = netInfo.getType();
+
+                    //#mdebug debug
+                    System.out.println(">>> SocketBroadcastReceiver: state = " + state +
+                            " detail = " + netInfo.getDetailedState() + " type = " + type +
+                            " name = " + netInfo.getTypeName());
+                    //#enddebug
+
+                    if (type != networkType || state != networkState) {
+                        networkType = type;
+                        networkState = state;
+                        cleanSocketConnections(closeConnections);
+                    }
+                }
+            }
+        }
+
+        private void cleanSocketConnections(boolean closeConnections) {
+            //#debug debug
+            System.out.println(">>> SocketBroadcastReceiver: closeSocketConnections() " + socketWeakList.size());
+
+            // NOTE: Looping from the end of the vector, so we can safely delete vector elements
+            for (int i = socketWeakList.size() - 1; i >= 0; i--) {
+                SocketConnection conn = socketWeakList.elementAt(i).get();
+                if (closeConnections) {
+                    if (conn != null) {
+                        try {
+                            conn.close();
+                        } catch (Throwable e) {
+                        }
+                    }
+                    conn = null;
+                }
+
+                if (conn == null) {
+                    socketWeakList.removeElementAt(i);
+                }
+            }
+
+            if (closeConnections) {
+                synchronized (ConnectivityBroadcastReceiver.class) {
+                    if (socketBroadcastReceiver != null && socketWeakList.size() == 0) {
+                        //#debug debug
+                        System.out.println(">>> SocketBroadcastReceiver: close");
+                        AndroidMeActivity.DEFAULT_ACTIVITY.unregisterReceiver(socketBroadcastReceiver);
+                        socketBroadcastReceiver = null;
+                    }
+                }
+            }
+        }
+    }
 }
