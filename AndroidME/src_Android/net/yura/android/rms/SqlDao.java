ee8cc48d30b3f410fe041c6f08e4d56927210493
yuranet <yuranet@50c89ffe-5e77-4af1-8422-6b4edaf2f3c0>
2010-12-08 13:17:24 +0000

    endTransaction is in a finally

--- AndroidME/src_Android/net/yura/android/rms/SqlDao.java
+++ AndroidME/src_Android/net/yura/android/rms/SqlDao.java
@@ -344,6 +344,7 @@ public class SqlDao {
             }
         }
         catch (Exception e) {
+            e.printStackTrace();
             // Do nothing. Just return null...
         }
         finally {
