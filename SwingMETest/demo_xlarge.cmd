4c65d7212f48c8631a73bd3ff695665e68230141
yuranet <yuranet@50c89ffe-5e77-4af1-8422-6b4edaf2f3c0>
2011-01-06 16:05:43 +0000

    icons test added

--- SwingMETest/demo_xlarge.cmd
+++ SwingMETest/demo_xlarge.cmd
@@ -1,3 +1,3 @@
 rem N80
-java -cp lib/midletrunner.jar;lib/SwingME.jar;dist/SwingMETest.jar org.me4se.MIDletRunner -width 352 -height 450 net.yura.mobile.test.MyMidlet
+java -cp lib/midletrunner.jar;lib/SwingME.jar;lib/m3gbasic.jar;dist/SwingMETest.jar org.me4se.MIDletRunner -width 352 -height 450 net.yura.mobile.test.MyMidlet
 pause
