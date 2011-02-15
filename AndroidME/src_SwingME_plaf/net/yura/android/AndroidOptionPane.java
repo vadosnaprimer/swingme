4ed95a8d1ed0dbdd3b6ab8f9d1a86c372e6ef507
yuranet <yuranet@50c89ffe-5e77-4af1-8422-6b4edaf2f3c0>
2011-02-15 18:20:31 +0000

    fallback added for android optionpane

--- AndroidME/src_SwingME_plaf/net/yura/android/AndroidOptionPane.java
+++ AndroidME/src_SwingME_plaf/net/yura/android/AndroidOptionPane.java
@@ -6,19 +6,30 @@ import net.yura.mobile.logging.Logger;
 
 public class AndroidOptionPane extends OptionPane {
 
+    // Make sure we have unique url's, so we can have as many activities (pop-ups) as needed.
     public static int optionPaneCounter;
 
     @Override
     public void setVisible(boolean b) {
         if (getMessage() instanceof String) {
             if (b) {
-                // Make sure we have unique url's, so we can have as many activities (pop-ups) as needed.
-                optionPaneCounter++;
-                Midlet.getMidlet().platformRequest("nativeNoResult://"+OptionPaneActivity.class.getName()+"/" + optionPaneCounter, this);
+
+                try {
+                    Midlet.getMidlet().platformRequest("nativeNoResult://"+OptionPaneActivity.class.getName()+"/" + (optionPaneCounter++), this);
+                }
+                catch(Exception ex) {
+                    //#debug debug
+                    ex.printStackTrace();
+
+                    //#debug warn
+                    Logger.warn("failed to start OptionPaneActivity, falling back to SwingME OptionPane");
+                    super.setVisible(b);
+                }
             }
             else {
                 //#debug warn
-                Logger.warn("why is this happening???? setVisible(false) in AndroidOptionPane");
+                Logger.warn("this should never happen!!!! setVisible(false) in AndroidOptionPane");
+                super.setVisible(b);
             }
         }
         else {
