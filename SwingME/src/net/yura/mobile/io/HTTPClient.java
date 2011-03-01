844579e70838c9431ee6a009ed77fc2b3f4b8ef9
yuranet <yuranet@50c89ffe-5e77-4af1-8422-6b4edaf2f3c0>
2011-03-01 10:48:53 +0000

    url decode added

--- SwingME/src/net/yura/mobile/io/HTTPClient.java
+++ SwingME/src/net/yura/mobile/io/HTTPClient.java
@@ -183,6 +183,9 @@ public abstract class HTTPClient extends QueueProcessorThread {
         }
     }
 
+    /**
+     * @see java.net.URLEncoder#encode(java.lang.String, java.lang.String) URLEncoder.encode
+     */
     public static String encode(String s) {
         byte[] bytes = s.getBytes();
         StringBuffer ret = new StringBuffer(s.length());
@@ -213,4 +216,79 @@ public abstract class HTTPClient extends QueueProcessorThread {
         }
         buff.append(Integer.toHexString(arg0));
     }
+
+
+    /**
+     * @see java.net.URLDecoder#decode(java.lang.String, java.lang.String) URLDecoder.decode
+     */
+    public static String decode(String s) {
+
+	boolean needToChange = false;
+	int numChars = s.length();
+	StringBuffer sb = new StringBuffer(numChars > 500 ? numChars / 2 : numChars);
+	int i = 0;
+
+	char c;
+	byte[] bytes = null;
+	while (i < numChars) {
+            c = s.charAt(i);
+            switch (c) {
+	    case '+':
+		sb.append(' ');
+		i++;
+		needToChange = true;
+		break;
+	    case '%':
+		/*
+		 * Starting with this instance of %, process all
+		 * consecutive substrings of the form %xy. Each
+		 * substring %xy will yield a byte. Convert all
+		 * consecutive  bytes obtained this way to whatever
+		 * character(s) they represent in the provided
+		 * encoding.
+		 */
+
+		try {
+
+		    // (numChars-i)/3 is an upper bound for the number
+		    // of remaining bytes
+		    if (bytes == null)
+			bytes = new byte[(numChars-i)/3];
+		    int pos = 0;
+
+		    while ( ((i+2) < numChars) &&
+			    (c=='%')) {
+			bytes[pos++] =
+			    (byte)Integer.parseInt(s.substring(i+1,i+3),16);
+			i+= 3;
+			if (i < numChars)
+			    c = s.charAt(i);
+		    }
+
+		    // A trailing, incomplete byte encoding such as
+		    // "%x" will cause an exception to be thrown
+
+		    if ((i < numChars) && (c=='%'))
+			throw new IllegalArgumentException(
+		         "URLDecoder: Incomplete trailing escape (%) pattern");
+
+		    sb.append(new String(bytes, 0, pos));
+		} catch (NumberFormatException e) {
+		    throw new IllegalArgumentException(
+                    "URLDecoder: Illegal hex characters in escape (%) pattern - "
+		    + e.getMessage());
+		}
+		needToChange = true;
+		break;
+	    default:
+		sb.append(c);
+		i++;
+		break;
+            }
+        }
+
+        return (needToChange? sb.toString() : s);
+    }
+
+
 }
