0ec8ad1d697b7747748bbde7756faa35f218fa7c
janeroski <janeroski@50c89ffe-5e77-4af1-8422-6b4edaf2f3c0>
2011-05-11 09:45:18 +0000

    Indeterminate Spinner: Make it closer to native. Some phones had 12 frames, others 16. Better hack. No longer needs to know the exact number of frames.

--- AndroidME/src_SwingME_plaf/net/yura/android/plaf/AndroidSprite.java
+++ AndroidME/src_SwingME_plaf/net/yura/android/plaf/AndroidSprite.java
@@ -18,46 +18,54 @@ public class AndroidSprite extends Sprite {
 
     @Override
     public void paint(Graphics g) {
-        android.graphics.Canvas canvas = g.getCanvas();
-        int x = getX();
-        int y = getY();
-        int width = getWidth();
-        int height = getHeight();
-        //spin.set
         Drawable draw;
-
-        canvas.save();
 
         if (spin instanceof AnimationDrawable) {
             draw = ((AnimationDrawable)spin).getFrame(frame);
         }
-        else if (spin.getClass().getName().equals("android.graphics.drawable.AnimatedRotateDrawable")) {
-            int totalFrame = getFrameSequenceLength();
-            canvas.rotate((frame*360)/totalFrame, x+width/2, y+height/2);
+        else if (spin instanceof Runnable) {
             draw = spin;
         }
+//        else if (spin.getClass().getName().equals("android.graphics.drawable.AnimatedRotateDrawable")) {
+//            int totalFrame = getFrameSequenceLength();
+//            canvas.rotate((frame*360)/totalFrame, x+width/2, y+height/2);
+//            draw = spin;
+//        }
         else {
             draw = spin;
             System.out.println("DO NOT KNOW HOW TO DRAW "+spin);
         }
-        draw.setBounds(x, y, x+width, y+height);
+
+        android.graphics.Canvas canvas = g.getCanvas();
+        canvas.save();
+
+        draw.setBounds(0, 0, getWidth(), getHeight());
+        canvas.translate(getX(), getY());
         draw.draw(canvas);
 
         canvas.restore();
     }
+
     @Override
     public int getFrameSequenceLength() {
         if (spin instanceof AnimationDrawable) {
             return ((AnimationDrawable)spin).getNumberOfFrames();
         }
-        if (spin.getClass().getName().equals("android.graphics.drawable.AnimatedRotateDrawable")) {
-            return 12;
-        }
-        return 1;
-    }
-    @Override
-    public void setFrame(int frame) {
-        this.frame = frame;
+//        if (spin.getClass().getName().equals("android.graphics.drawable.AnimatedRotateDrawable")) {
+//            return 12;
+//        }
+        return 12; // Android default implementation value.
     }
 
+    @Override
+    public void setFrame(int frame) {
+        // HACK: android.graphics.drawable.AnimatedRotateDrawable class implements
+        // Runnable. Calling run() will make it rotate one position. We are
+        // ignoring the requesting frame, but that is OK for a spinner.
+        if (this.frame != frame && spin instanceof Runnable) {
+            ((Runnable) spin).run();
+        }
+
+        this.frame = frame;
+    }
 }
