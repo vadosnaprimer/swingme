aa1e28c0b914f0a08be262b7d1da4008cbe26f0c
antonlaurens <antonlaurens@50c89ffe-5e77-4af1-8422-6b4edaf2f3c0>
2011-07-19 08:48:31 +0000

    Blackberry native camera screen

---
+++ BlackBerryME/src/net/yura/blackberry/CameraInvoker.java
@@ -0,0 +1,72 @@
+package net.yura.blackberry;
+
+
+import net.rim.blackberry.api.invoke.CameraArguments;
+import net.rim.blackberry.api.invoke.Invoke;
+import net.rim.device.api.io.file.FileSystemJournal;
+import net.rim.device.api.io.file.FileSystemJournalEntry;
+import net.rim.device.api.io.file.FileSystemJournalListener;
+import net.rim.device.api.system.Characters;
+import net.rim.device.api.system.EventInjector;
+import net.rim.device.api.ui.UiApplication;
+import net.yura.mobile.gui.Midlet;
+import net.yura.mobile.logging.Logger;
+
+public class CameraInvoker implements com.badoo.mobile.BlackBerryNativeScreen , FileSystemJournalListener {
+
+	long _lastUSN;
+	String capturedImgPath = "";
+	byte[] ImageData = null;
+	String full_path;
+
+	public CameraInvoker() {
+		UiApplication.getUiApplication().addFileSystemJournalListener(this);
+		_lastUSN = FileSystemJournal.getNextUSN();
+	}
+
+	public void show() {
+		Logger.info("Invoking camera");
+		Invoke.invokeApplication(Invoke.APP_TYPE_CAMERA, new CameraArguments());
+	}
+
+	public void fileJournalChanged() {
+		long nextUSN = FileSystemJournal.getNextUSN();
+		String msg = null;
+		String path = null;
+		for (long lookUSN = nextUSN - 1; lookUSN >= _lastUSN && msg == null; --lookUSN) {
+			FileSystemJournalEntry entry = FileSystemJournal.getEntry(lookUSN);
+			if (entry == null) {
+				break;
+			}
+			path = entry.getPath();
+			if (entry.getEvent() == FileSystemJournalEntry.FILE_ADDED) {
+				switch (entry.getEvent()) {
+				case FileSystemJournalEntry.FILE_ADDED:
+					if (path.indexOf(".jpg") > 0) {
+						Logger.info("New image captured with camera: " + path);
+						capturedImgPath = path;
+						UiApplication.getUiApplication().removeFileSystemJournalListener(this);
+						closeCamera();
+					}
+					break;
+				}
+			}
+		}
+		_lastUSN = nextUSN;
+	}
+
+	private void closeCamera() {
+		Midlet.getMidlet().onResult(-1, capturedImgPath);
+		try {
+			EventInjector.KeyEvent inject = new EventInjector.KeyEvent(EventInjector.KeyEvent.KEY_DOWN, Characters.ESCAPE,  50);
+			inject.post();
+			inject.post();
+		} catch (Exception e) {
+			Logger.warn("Could not close camera with injected events: " + e.getMessage());
+		}
+	}
+
+	public Object getResult() {
+		return null;
+	}
+}
