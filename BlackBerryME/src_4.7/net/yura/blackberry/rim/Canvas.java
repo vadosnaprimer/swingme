8d615af400eefef06860ba5c576cc409c5dd90e9
yuranet <yuranet@50c89ffe-5e77-4af1-8422-6b4edaf2f3c0>
2011-08-15 13:32:36 +0000

    multi touch added

--- BlackBerryME/src_4.7/net/yura/blackberry/rim/Canvas.java
+++ BlackBerryME/src_4.7/net/yura/blackberry/rim/Canvas.java
@@ -461,9 +461,17 @@ public abstract class Canvas extends FullScreen {
 	    }
 	
 	
-	
+	    // copy and paste from SwingME DesktopPane
+	    private static final int DRAGGED = 0;
+	    private static final int PRESSED = 1;
+	    private static final int RELEASED = 2;
+	    //private static final int CANCEL = 3;
+
+	    int x1=-1,x2=-1,y1=-1,y2=-1;
 	
 	protected boolean touchEvent(TouchEvent message) {
+
+	    
 	    
 	    int event = message.getEvent();
             int x = message.getGlobalX(1);
@@ -497,16 +505,16 @@ public abstract class Canvas extends FullScreen {
 //            		if (event == TouchEvent.CLICK) {
 	                if (event == TouchEvent.DOWN) {
 	                        pointerPressed( x, y );
-	                        return true;
+	                        //return true;
 //	                } else if (event == TouchEvent.UNCLICK) {	                        
 	                } else if (event == TouchEvent.UP) {
 	                        pointerReleased( x, y );
-	                        return true;
+	                        //return true;
 	                } else {
 	                        if (event == TouchEvent.MOVE) {
 	                                pointerDragged( x, y );
-	                                invalidate();
-	                                return true;
+	                                //invalidate();
+	                                //return true;
 	                        }
 	                        
 	                        // maybe 1 day support the touch of the storm
@@ -531,7 +539,55 @@ public abstract class Canvas extends FullScreen {
 	                //if (isSuperCalled) {
 	                //        return false;
 	                //}
-	        return super.touchEvent(message);
+	                
+	                
+	                
+	                
+	                
+	                
+	                int x1 = message.getX(1);
+	                int y1 = message.getY(1);
+	                
+	                int x2 = message.getX(2);
+	                int y2 = message.getY(2);
+	                
+	                if (x1>=0 && y1>=0 && x2>=0 && y2>=0) {
+	                    //int event = message.getEvent();
+	                    int swingEvent;
+	                    //switch (event) {
+	                    //    case TouchEvent.DOWN: swingEvent = PRESSED; break;
+	                    //    case TouchEvent.UP: swingEvent = RELEASED; break;
+	                    //    case TouchEvent.MOVE: swingEvent = DRAGGED; break;
+	                    //}
+	                    if (this.x1<0 || this.y1<0 || this.x2<0 || this.y2<0) {
+	                        swingEvent = PRESSED;
+	                    }
+	                    else {
+	                        swingEvent = DRAGGED;
+	                    }
+	                    multitouchEvent(new int[]{swingEvent,swingEvent}, new int[]{x1,x2}, new int[]{y1,y2});
+	                }
+	                else if (this.x1>=0 && this.y1>=0 && this.x2>=0 && this.y2>=0) {
+	                    multitouchEvent(new int[]{RELEASED,RELEASED}, new int[]{this.x1,this.x2}, new int[]{this.y1,this.y2});
+	                }
+	                else {
+	                    //#debug debug
+	                    System.out.println("BB multitouchEvent skip "+message.getEvent()+" point1="+x1+" "+y1+" point2="+x2+" "+y2);
+	                }
+	                
+	                this.x1=x1;
+	                this.y1=y1;
+	                this.x2=x2;
+	                this.y2=y2;
+	                
+	                
+	                
+	                
+	                
+	                
+	                
+	                
+	        return true;//super.touchEvent(message);
 	    }
 	
 	    protected boolean navigationClick(int status, int time)
