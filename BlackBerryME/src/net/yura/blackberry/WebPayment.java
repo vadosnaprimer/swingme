09ad855731aa78910f873cc5f1c2894fe8c14235
antonlaurens <antonlaurens@50c89ffe-5e77-4af1-8422-6b4edaf2f3c0>
2011-11-16 14:34:24 +0000

    Try all types of WAP connections first, before reverting to BIS-B and WIFI

--- BlackBerryME/src/net/yura/blackberry/WebPayment.java
+++ BlackBerryME/src/net/yura/blackberry/WebPayment.java
@@ -15,46 +15,44 @@ import org.w3c.dom.Document;
 
 public final class WebPayment extends MainScreen {
 
-        private BrowserField browserField;
+    private BrowserField browserField;
 
-        public WebPayment(String startURL, final String successURL, final String errorURL, final int successConstant, final int errorConstant, String title, boolean tryUseCarrierConnection) {
-                setTitle(title);
-                BrowserFieldConfig myBrowserFieldConfig = new BrowserFieldConfig();
+    public WebPayment(String startURL, final String successURL, final String errorURL, final int successConstant, final int errorConstant, String title, boolean tryUseCarrierConnection) {
+        setTitle(title);
+        BrowserFieldConfig myBrowserFieldConfig = new BrowserFieldConfig();
 
-                if (tryUseCarrierConnection) {
-                        if (TransportInfo.isTransportTypeAvailable(TransportInfo.TRANSPORT_WAP2)) {
-                                ConnectionFactory connectionFactory = new ConnectionFactory();
-                                int[] transportTypes = { TransportInfo.TRANSPORT_WAP2 };
-                                connectionFactory.setPreferredTransportTypes(transportTypes);
-                                myBrowserFieldConfig.setProperty(BrowserFieldConfig.CONNECTION_FACTORY, connectionFactory);
-                        }
-                }
-
-                myBrowserFieldConfig.setProperty(BrowserFieldConfig.NAVIGATION_MODE, BrowserFieldConfig.NAVIGATION_MODE_POINTER);
-                browserField = new BrowserField(myBrowserFieldConfig);
-                add(browserField);
-                browserField.requestContent(startURL);
-                browserField.addListener(new BrowserFieldListener() {
-                        public void documentCreated(BrowserField browserField, ScriptEngine scriptEngine, Document document) {
-                                if (document.getBaseURI().toLowerCase().startsWith(successURL.toLowerCase())) {
-                                        Midlet.getMidlet().onResult(-1, successConstant, null);
-                                        close();
-                                }
-                                else if (document.getBaseURI().toLowerCase().startsWith(errorURL.toLowerCase())) {
-                                        Midlet.getMidlet().onResult(-1, errorConstant, null);
-                                        close();
-                                }
-                                System.out.println("Document created. URI: " + document.getBaseURI());
-                        }
-                });
-                UiApplication.getUiApplication().pushScreen(this);
+        if (tryUseCarrierConnection) {
+            ConnectionFactory connectionFactory = new ConnectionFactory();
+            int[] transportTypes = { TransportInfo.TRANSPORT_WAP2, TransportInfo.TRANSPORT_WAP, TransportInfo.TRANSPORT_TCP_CELLULAR, TransportInfo.TRANSPORT_BIS_B, TransportInfo.TRANSPORT_TCP_WIFI };
+            connectionFactory.setPreferredTransportTypes(transportTypes);
+            myBrowserFieldConfig.setProperty(BrowserFieldConfig.CONNECTION_FACTORY, connectionFactory);
         }
 
-        protected boolean keyDown(int keyCode, int time) {
-                if (Keypad.key(keyCode) == Keypad.KEY_ESCAPE) {
-                        return browserField.back();
+        myBrowserFieldConfig.setProperty(BrowserFieldConfig.NAVIGATION_MODE, BrowserFieldConfig.NAVIGATION_MODE_POINTER);
+        browserField = new BrowserField(myBrowserFieldConfig);
+        add(browserField);
+        browserField.requestContent(startURL);
+        browserField.addListener(new BrowserFieldListener() {
+            public void documentCreated(BrowserField browserField, ScriptEngine scriptEngine, Document document) {
+                if (document.getBaseURI().toLowerCase().startsWith(successURL.toLowerCase())) {
+                    Midlet.getMidlet().onResult(-1, successConstant, null);
+                    close();
                 }
-                return super.keyDown(keyCode, time);
+                else if (document.getBaseURI().toLowerCase().startsWith(errorURL.toLowerCase())) {
+                    Midlet.getMidlet().onResult(-1, errorConstant, null);
+                    close();
+                }
+                System.out.println("Document created. URI: " + document.getBaseURI());
+            }
+        });
+        UiApplication.getUiApplication().pushScreen(this);
+    }
+
+    protected boolean keyDown(int keyCode, int time) {
+        if (Keypad.key(keyCode) == Keypad.KEY_ESCAPE) {
+            return browserField.back();
         }
+        return super.keyDown(keyCode, time);
+    }
 
 }
