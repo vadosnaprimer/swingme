68ce72a4131c9549ff4903cafd20db5d0a100a77
janeroski <janeroski@50c89ffe-5e77-4af1-8422-6b4edaf2f3c0>
2011-12-12 13:09:41 +0000

    RemoteTest: clicking by index (clickFocusable)

--- AndroidME/src_SwingME_plaf/net/yura/android/AndroidRemoteTest.java
+++ AndroidME/src_SwingME_plaf/net/yura/android/AndroidRemoteTest.java
@@ -5,6 +5,7 @@ import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.util.Enumeration;
+import java.util.Vector;
 
 import net.yura.mobile.util.RemoteTest;
 import android.app.Instrumentation;
@@ -13,19 +14,19 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Checkable;
 import android.widget.TextView;
 
 public class AndroidRemoteTest extends RemoteTest {
 
     public AndroidRemoteTest() {
-
         Log.d("YURA", "my ip "+getLocalIpAddress());
-
     }
 
     @Override
     protected boolean onClickText(String text) {
-        View window = getSelectedFrame(); // TODO can this be null???
+        View window = getSelectedFrame();
 
         //System.out.println("Window="+toString(window));
 
@@ -34,6 +35,47 @@ public class AndroidRemoteTest extends RemoteTest {
             return super.onClickText(text); // Do SwingME click
         }
         return nativeClick;
+    }
+
+    @Override
+    protected boolean onClickFocusable(final int n1, final int n2) {
+        if (AndroidMeActivity.DEFAULT_ACTIVITY.hasWindowFocus()) {
+            return super.onClickFocusable(n1, n2);
+        }
+
+        View window = getSelectedFrame();
+
+        Vector<View> focusList = new Vector<View>();
+        getFocusableViews(window, focusList);
+
+        boolean isValidComponent = (n1 >= 0 && n1 < focusList.size());
+        if (isValidComponent) {
+            View view = focusList.elementAt(n1);
+            if (view instanceof AdapterView) {
+                final AdapterView list = (AdapterView) view;
+                isValidComponent = (n2 >= 0 && n2 < list.getCount());
+
+                // Note: The item we are about to click may not be visible.
+                // performItemClick() seems to do the job but needs to run on
+                // the UI thread.
+                if (isValidComponent) {
+                    AndroidMeActivity.DEFAULT_ACTIVITY.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                list.performItemClick(null, n2, 0);
+                            }
+                            catch (Throwable e) {}
+                        }
+                    });
+                }
+            }
+            else {
+                click(view);
+            }
+        }
+
+        return isValidComponent;
     }
 
     @Override
@@ -50,14 +92,14 @@ public class AndroidRemoteTest extends RemoteTest {
     }
 
     private static boolean clickText(View view, String clickText) {
-        if (view instanceof ViewGroup) {
+        if (view.isShown() && view instanceof ViewGroup) {
             ViewGroup viewGroup = (ViewGroup) view;
 
             int childcount = viewGroup.getChildCount();
             for (int i=0; i < childcount; i++){
                 View childView = viewGroup.getChildAt(i);
 
-                if (childView instanceof TextView && childView.isClickable()) {
+                if (childView instanceof TextView && childView.isClickable() && childView.isShown()) {
                     TextView b = (TextView) childView;
                     if (clickText.equalsIgnoreCase(b.getText().toString())) {
                         click(b);
@@ -74,6 +116,28 @@ public class AndroidRemoteTest extends RemoteTest {
         return false;
     }
 
+    private static void getFocusableViews(View view, Vector<View> focusList) {
+        if (view.isShown()) {
+            if (view instanceof AdapterView) { // Always add lists
+                focusList.add(view);
+            }
+            else if (view instanceof ViewGroup) {
+                ViewGroup viewGroup = (ViewGroup) view;
+
+                int childcount = viewGroup.getChildCount();
+                for (int i=0; i < childcount; i++) {
+                    // recursive call
+                    getFocusableViews(viewGroup.getChildAt(i), focusList);
+                }
+            }
+            else if (view.isClickable() || view instanceof Checkable) {
+                // Note: for some weird reason CheckedTextView returns that it's not
+                // clickable... but it is. Other Checkable's may be doing he same.
+                focusList.add(view);
+            }
+        }
+    }
+
     public static void click(View view) {
         int[] xy = new int[2];
         view.getLocationOnScreen(xy);
@@ -83,9 +147,8 @@ public class AndroidRemoteTest extends RemoteTest {
     public static void click(float x, float y) {
         Instrumentation inst = new Instrumentation();
         long downTime = SystemClock.uptimeMillis();
-        long eventTime = SystemClock.uptimeMillis();
-        MotionEvent event = MotionEvent.obtain(downTime, eventTime, MotionEvent.ACTION_DOWN, x, y, 0);
-        MotionEvent event2 = MotionEvent.obtain(downTime, eventTime, MotionEvent.ACTION_UP, x, y, 0);
+        MotionEvent event = MotionEvent.obtain(downTime, downTime, MotionEvent.ACTION_DOWN, x, y, 0);
+        MotionEvent event2 = MotionEvent.obtain(downTime, downTime, MotionEvent.ACTION_UP, x, y, 0);
         inst.sendPointerSync(event);
         inst.sendPointerSync(event2);
     }
@@ -94,9 +157,7 @@ public class AndroidRemoteTest extends RemoteTest {
         StringBuilder builder = new StringBuilder();
         builder.append( view.getClass().getSimpleName() );
         if (view instanceof TextView) {
-            builder.append(" [");
-            builder.append( ((TextView)view).getText() );
-            builder.append("]");
+            builder.append(" [").append(((TextView)view).getText()).append("]");
         }
         if (view instanceof ViewGroup) {
             builder.append(" [");
