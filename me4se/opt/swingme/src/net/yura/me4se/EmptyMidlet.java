5177284fb40654e645ce667346720d8532232327
yuranet <yuranet@50c89ffe-5e77-4af1-8422-6b4edaf2f3c0>
2011-12-14 17:26:10 +0000

    opt-swingme added

---
+++ me4se/opt/swingme/src/net/yura/me4se/EmptyMidlet.java
@@ -0,0 +1,12 @@
+package net.yura.me4se;
+
+import net.yura.mobile.gui.DesktopPane;
+import net.yura.mobile.gui.Midlet;
+
+/**
+ * @author Yura Mamyrin
+ */
+public class EmptyMidlet extends Midlet {
+
+        protected void initialize(DesktopPane dp) { }
+}
---
+++ me4se/opt/swingme/src/net/yura/me4se/ME4SEPanel.java
@@ -0,0 +1,89 @@
+package net.yura.me4se;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Dimension;
+import javax.microedition.midlet.ApplicationManager;
+import net.yura.mobile.gui.DesktopPane;
+import net.yura.mobile.gui.components.Frame;
+import net.yura.mobile.gui.components.Panel;
+import net.yura.mobile.gui.plaf.nimbus.NimbusLookAndFeel;
+import org.me4se.JadFile;
+
+/**
+ * @author Yura Mamyrin
+ */
+public class ME4SEPanel extends Container {
+
+    private ApplicationManager manager;
+    private DesktopPane desktop;
+    private Frame frame1;
+
+    public DesktopPane getDesktopPane() {
+        return desktop;
+    }
+
+    public ME4SEPanel() {
+        setLayout( new BorderLayout() );
+
+        // this can only be happening in 1 thread at a time
+
+        manager = ApplicationManager.createInstance(this, null );
+
+        JadFile jad = new JadFile();
+        jad.setValue("MIDlet-1", ",," + EmptyMidlet.class.getName());
+        manager.launch(jad, 0);
+
+        desktop = DesktopPane.getDesktopPane();
+
+        // todo find this from a better place
+        desktop.setLookAndFeel( new NimbusLookAndFeel() );
+
+    }
+
+    public ApplicationManager getApplicationManager() {
+        return manager;
+    }
+
+    //public void destroy() {
+    //    can not use this as causes System.exit in all events
+    //    manager.destroy(true, false); // true will cause System.exit
+    //}
+
+    public java.awt.Component getComponent() {
+        return getComponents()[0];
+    }
+
+    public void add(Panel panel) {
+
+        frame1 = new Frame();
+        frame1.setUndecorated(true);
+        frame1.add(panel);
+        frame1.setMaximum(true);
+
+        desktop.add(frame1);
+    }
+
+    public Dimension getMinimumSize() {
+        Dimension d = super.getPreferredSize();
+
+        if (frame1!=null) {
+            Dimension a = new Dimension( frame1.getWidth(), frame1.getHeight());
+            frame1.pack(); // TODO: does this really set the height right away to the min???
+            Dimension b = new Dimension( frame1.getWidth(), frame1.getHeight());
+            //System.out.println("d="+d);
+            frame1.setSize(a.width, a.height);
+            return b;
+        }
+
+        return d;
+    }
+
+    public Dimension getPreferredSize() {
+        Dimension d = super.getPreferredSize();
+        if (d==null || d.width == 0 || d.height == 0) {
+            return getMinimumSize();
+        }
+        return d;
+    }
+}
