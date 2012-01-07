6b4679589a397cd919daf25fe25ea2bdd1d53180
yuranet <yuranet@50c89ffe-5e77-4af1-8422-6b4edaf2f3c0>
2012-01-07 22:11:50 +0000

    UTF-8 reader fix

--- UtilME/src/net/yura/mobile/io/UTF8InputStreamReader.java
+++ UtilME/src/net/yura/mobile/io/UTF8InputStreamReader.java
@@ -23,93 +23,95 @@ public class UTF8InputStreamReader extends Reader {
 
     public int read(char[] cbuf, int off, int len) throws IOException {
 
-        //len = 66;
-
-        if (buffer.length < len) {
-            buffer = new byte[len];
-        }
-
-        int available = inputStream.available();
-
-        // dont block trying to over-read, this is the line that the S60 InputStreamReader misses
-        int l = inputStream.read(buffer,0,available>len?len: (available==0?1:available) );
-
-        if (l<0) {
-            return l;
-        }
-
         int numChars = 0;
-        
-        for (int i=0;i<l;i++) {
 
-            int result=-1;
+        while (numChars == 0) { // we can NOT return a result of 0, so we will keep trying
 
-            if (stage == 0) {
+            if (buffer.length < len) {
+                buffer = new byte[len];
+            }
 
-                // get next byte unsigned
-                int b = buffer[ i ] & 0xff;
-                // classify based on the high order 3 bits
-                switch ( b >>> 5 ) {
-                    case 6:
-                        // two byte encoding
-                        // 110yyyyy 10xxxxxx
-                        // use low order 6 bits
-                        y = b & 0x1f;
-                        // use low order 6 bits of the next byte
-                        // It should have high order bits 10, which we don't check.
-                        stage = 1;
-                        continue;
-                    case 7:
-                        // three byte encoding
-                        // 1110zzzz 10yyyyyy 10xxxxxx
-                        if ( ( b & 0x10 ) != 0 ) throw new IOException("UTF8Decoder does not handle 32-bit characters");
-                        // use low order 4 bits
-                        z = b & 0x0f;
-                        // use low order 6 bits of the next byte
-                        // It should have high order bits 10, which we don't check.
-                        stage = 2;
-                        continue;
-                    default:
-                        // one byte encoding
-                        // 0xxxxxxx
-                        // use just low order 7 bits
-                        // 00000000 0xxxxxxx
-                        result = ( char ) ( b & 0x7f );
-                        break;
+            int available = inputStream.available();
+
+            // dont block trying to over-read, this is the line that the S60 InputStreamReader misses
+            int l = inputStream.read(buffer,0,available>len?len: (available==0?1:available) );
+
+            if (l<0) {
+                return l;
+            }
+
+            for (int i=0;i<l;i++) {
+
+                int result=-1;
+
+                if (stage == 0) {
+
+                    // get next byte unsigned
+                    int b = buffer[ i ] & 0xff;
+                    // classify based on the high order 3 bits
+                    switch ( b >>> 5 ) {
+                        case 6:
+                            // two byte encoding
+                            // 110yyyyy 10xxxxxx
+                            // use low order 6 bits
+                            y = b & 0x1f;
+                            // use low order 6 bits of the next byte
+                            // It should have high order bits 10, which we don't check.
+                            stage = 1;
+                            continue;
+                        case 7:
+                            // three byte encoding
+                            // 1110zzzz 10yyyyyy 10xxxxxx
+                            if ( ( b & 0x10 ) != 0 ) throw new IOException("UTF8Decoder does not handle 32-bit characters");
+                            // use low order 4 bits
+                            z = b & 0x0f;
+                            // use low order 6 bits of the next byte
+                            // It should have high order bits 10, which we don't check.
+                            stage = 2;
+                            continue;
+                        default:
+                            // one byte encoding
+                            // 0xxxxxxx
+                            // use just low order 7 bits
+                            // 00000000 0xxxxxxx
+                            result = ( char ) ( b & 0x7f );
+                            break;
+                    }
                 }
+                else if (stage == 1) {
+
+                            x = buffer[ i ] & 0x3f;
+                            // 00000yyy yyxxxxxx
+                            result = ( char ) ( y << 6 | x );
+
+
+                }
+                else if (stage == 2) {
+
+                            y = buffer[ i ] & 0x3f;
+                            // use low order 6 bits of the next byte
+                            // It should have high order bits 10, which we don't check.
+                            stage = 3;
+                            continue;
+
+                }
+                else if (stage==3) {
+
+                            x = buffer[ i ] & 0x3f;
+                            // zzzzyyyy yyxxxxxx
+                            result = ( char ) ( z << 12 | y << 6 | x );
+
+                }
+
+                if (result == -1) throw new IOException();
+                cbuf[off + numChars] = (char)result;
+                numChars++;
+                stage = 0;
+
             }
-            else if (stage == 1) {
-
-                        x = buffer[ i ] & 0x3f;
-                        // 00000yyy yyxxxxxx
-                        result = ( char ) ( y << 6 | x );
-
-
-            }
-            else if (stage == 2) {
-
-                        y = buffer[ i ] & 0x3f;
-                        // use low order 6 bits of the next byte
-                        // It should have high order bits 10, which we don't check.
-                        stage = 3;
-                        continue;
-
-            }
-            else if (stage==3) {
-
-                        x = buffer[ i ] & 0x3f;
-                        // zzzzyyyy yyxxxxxx
-                        result = ( char ) ( z << 12 | y << 6 | x );
-
-            }
-
-            if (result == -1) throw new IOException();
-            cbuf[off + numChars] = (char)result;
-            numChars++;
-            stage = 0;
-
+        
         }
-
+        
         return numChars;
 
     }
