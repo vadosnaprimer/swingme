0225785f2d9e4e4aabf55570f3fd4354e08be2fd
yuranet <yuranet@50c89ffe-5e77-4af1-8422-6b4edaf2f3c0>
2012-06-28 01:48:19 +0000

    more tests

--- SwingMETest/src/net/yura/mobile/junit/JMUnitTest.java
+++ SwingMETest/src/net/yura/mobile/junit/JMUnitTest.java
@@ -1,7 +1,11 @@
 package net.yura.mobile.junit;
 
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Hashtable;
 import java.util.Vector;
+import javax.microedition.io.Connector;
+import javax.microedition.io.StreamConnection;
 import jmunit.framework.cldc11.*;
 import net.yura.mobile.gui.layout.XULLoader;
 import net.yura.mobile.io.RMSUtil;
@@ -14,7 +18,7 @@ public class JMUnitTest extends TestCase {
     
     public JMUnitTest() {
         //The first parameter of inherited constructor is the number of test cases
-        super(2, "JMUnitTest");
+        super(3, "JMUnitTest");
     }    
     
     public void test(int testNumber) throws Throwable {
@@ -22,6 +26,7 @@ public class JMUnitTest extends TestCase {
         switch(testNumber) {
             case 0: testXULLoaderAdjustSizeToDensity(); break;
             case 1: testRMSUtil(); break;
+            case 2: testSocketGet(); break;
         }
         
     }
@@ -46,6 +51,9 @@ public class JMUnitTest extends TestCase {
         
         XULLoader.setDPI("xhdpi");
         assertEquals(96, XULLoader.adjustSizeToDensity(source) );
+        
+        XULLoader.setDPI("tvdpi");
+        assertEquals(64, XULLoader.adjustSizeToDensity(source) );
     }
     
     public void testRMSUtil() throws Exception {
@@ -64,4 +72,35 @@ public class JMUnitTest extends TestCase {
 
     }
     
+    
+    void testSocketGet() throws Exception {
+        
+/*
+        StreamConnection socket = (StreamConnection)Connector.open("socket://google.com:80");
+        //Socket socket = SSLSocketFactory.getDefault().createSocket("google.com", 443 );
+        //Socket socket = SocketFactory.getDefault().createSocket("google.com", 80 );
+
+        String post = "GET / HTTP/1.0\r\n" + // 1.1 does not close the InputStream
+        "Host: google.com\r\n" +
+        "User-Agent: Mozilla/5.0 (Windows NT 5.2; WOW64; rv:10.0.2) Gecko/20100101 Firefox/10.0.2\r\n" +
+        "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*€/*;q=0.8\r\n" +
+        // "Accept-Language: en-us,en;q=0.5\r\n" +
+        // "Accept-Encoding: gzip, deflate\r\n" +
+        // "Connection: keep-alive\r\n" +
+        // "Referer: http://yura.net/\r\n" +
+        "\r\n";
+
+        OutputStream out = socket.openOutputStream();
+        InputStream in = socket.openInputStream();
+
+        out.write( post.getBytes("ISO-8859-1") );
+        out.flush();
+
+        byte[] bytes = SystemUtil.getData(in, -1);
+
+        String result = new String(bytes,"ISO-8859-1");
+        System.out.println( result );
+*/
+        
+    }
 }
