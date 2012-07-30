cf1d9b6a1ca0c194acbfb6fec40b2891867a8fcd
yuranet <yuranet@50c89ffe-5e77-4af1-8422-6b4edaf2f3c0>
2012-07-30 11:50:31 +0000

    more testing

--- ToolsME/test/net/yura/tools/mobilegen/test/Test2.java
+++ ToolsME/test/net/yura/tools/mobilegen/test/Test2.java
@@ -2,9 +2,13 @@ package net.yura.tools.mobilegen.test;
 
 import com.google.protobuf.GeneratedMessage;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import net.yura.mobile.gen.ProtoAccess;
 import net.yura.server.gen.TestProtos;
 import net.yura.server.gen.TestProtos.Bob;
 import net.yura.server.gen.TestProtos.Test;
@@ -21,7 +25,7 @@ public class Test2 {
 
         Test test1 = Test.newBuilder().setId(55).build();
         TestObject.Builder test2 = TestObject.newBuilder().setId(5335).setAge(-5).setName("Gina");
-        test2.setHeads(5).setLastUpdated(0).setThings(TestProtos.OtherThingsType.stuff);
+        test2.setHeads(5).setLastUpdated(0).setThings(TestProtos.OtherThingsType.stuff).setIsAlive(false);
         Test test3 = Test.newBuilder().setId(53344335).build();
 
         Vector.Builder addressBook = Vector.newBuilder();
@@ -49,16 +53,31 @@ public class Test2 {
         bob.addAllVec3(vec1);
         
         Bob b = bob.build();
-System.out.println("b1="+b);
+
         byte[] bbytes = getObject(b).toByteArray();
 
         com.google.protobuf.GeneratedMessage obj = getObject( net.yura.server.gen.TestProtos.Object.parseFrom( new ByteArrayInputStream(bbytes) ) );
 
-System.out.println("b2="+obj);
-        Bob outbob = (Bob)obj;
-        Vector vout = outbob.getVec1(0); // 3 of them, also 3 vec2 and 3 vec3
+System.out.println("equals="+b.equals(obj)+" getting string: "+ ((Bob)obj).getVec1(0).getElements(3).getString() );
 
-System.out.println("equals="+b.equals(obj)+" getting string: "+ vout.getElements(3).getString() );
+
+        ProtoAccess access = new ProtoAccess();
+
+        Object out = access.load( new ByteArrayInputStream(bbytes) , bbytes.length );
+
+        System.out.println("out="+out);
+
+        // TODO check against sample object
+
+        ByteArrayOutputStream bout = new ByteArrayOutputStream();
+        access.save(bout, out);
+
+        com.google.protobuf.GeneratedMessage obj2 = getObject( net.yura.server.gen.TestProtos.Object.parseFrom( new ByteArrayInputStream(bout.toByteArray()) ) );
+        
+System.out.println("equals="+b.equals(obj2)+" getting string: "+ ((Bob)obj2).getVec1(0).getElements(3).getString() );
+
+
+
     }
 
     public static net.yura.server.gen.TestProtos.Object wrapString(String s) throws Exception {
